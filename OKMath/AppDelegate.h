78ac08387246616b85b03534ae626524d9f0001b
omer <omer@omers-Mac.local>
2014-11-22 12:37:08 -0800

    Initial Commit

---
+++ OKMath/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OKMath
+//
+//  Created by omer on 22/11/14.
+//  Copyright (c) 2014 omer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OKMath/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OKMath
+//
+//  Created by omer on 22/11/14.
+//  Copyright (c) 2014 omer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
